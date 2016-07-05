7dff63a79acf36bfea1b4a98d7ff803690633353
HarshenduBhardwaj <harshendubhardwaj@outlook.com>
2016-07-05 12:37:59 -0500

    Commit2

---
+++ src/test/java/resources/Util.java
@@ -0,0 +1,17 @@
+package resources;
+
+/**
+ * Created by HarshenduBhardwaj on 6/30/16.
+ */
+public class Util  {
+
+    public static final String baseUrl = "http://www.demo.guru99.com/V4/";
+    public static final String usernameLocator = "uid";
+    public static final String userName = "mngr40884";
+    public static final String passWordLocator = "password";
+    public static final String passWord = "YdYmanY";
+    public static final String passWord1 = "YdYmanY";
+    public static final String passWord2 = "YdYmanY";
+    public static final String submitButtonLocator = "btnLogin";
+    public static final String pageTitle = "Guru99 Bank Manager HomePage";
+}
