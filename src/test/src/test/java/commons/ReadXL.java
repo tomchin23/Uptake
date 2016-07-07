405929cd40df6af251d34a8ae89c2eb9262f364d
HarshenduBhardwaj <harshendubhardwaj@outlook.com>
2016-07-06 21:16:20 -0500

    Login

--- src/test/src/test/java/commons/ReadXL.java
+++ src/test/src/test/java/commons/ReadXL.java
@@ -6,7 +6,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
@@ -18,7 +17,7 @@ public class ReadXL {
 
     public static void main(String[] args) throws IOException {
 
-        InputStream ReadXL = new FileInputStream("C:\\Users\\hbhardwa\\Desktop\\OCA8\\Parameterize.xlsx");
+        InputStream ReadXL = new FileInputStream("/Users/HARSHENDU/Desktop/Parameterize.xlsx");
 
         XSSFWorkbook wb = new XSSFWorkbook(ReadXL);
         XSSFSheet sheet = wb.getSheet("Data");
@@ -31,30 +30,21 @@ public class ReadXL {
             row = (XSSFRow) IterateOnRows.next();
 
             Iterator IterateOnCells = row.cellIterator();
-
-            while(IterateOnCells.hasNext()) {
+            while (IterateOnCells.hasNext()) {
                 cell = (XSSFCell) IterateOnCells.next();
 
-
-                // Code above here enables the script to read the XL file.
-                // Code below here performs functions on the data read from the XL file.
-
-
-                if(cell.getCellType() ==  XSSFCell.CELL_TYPE_STRING) {
-                    System.out.println(cell.getStringCellValue());
+                if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+                    System.out.println(cell.getStringCellValue() + " ");
+                } else if (cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+                    System.out.println(cell.getNumericCellValue() + " ");
+                } else {
+                    System.out.println("It's all just blanks");
                 }
-                else if (cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC){
-                        System.out.println(cell.getNumericCellValue());
-                }
-
-                else
-                    System.out.println("No Values Present");
-            }
-
-            System.out.println();
+                System.out.println();
 
             }
         }
     }
+}
 
 
