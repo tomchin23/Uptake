1bcbfe2dc08f69af4de243e0f95fd68ef48f0e54
HarshenduBhardwaj <harshendubhardwaj@outlook.com>
2016-07-08 15:28:48 -0500

    Util.java

---
+++ src/test/java/resources/Util.java
@@ -0,0 +1,37 @@
+package resources;
+
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+
+import static resources.Drivers.driver;
+
+/**
+ * Created by HarshenduBhardwaj on 6/30/16.
+ */
+public class Util {
+
+    static Random randInt = new Random();
+
+
+    public static void getScreenShot() throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        FileUtils.copyFile(scrFile, new File("/Users/hbhardwa/Desktop/OCA/ScreenShot_" + randInt.nextInt(1000) + ".png"));
+    }
+
+    public static void End() {
+
+        driver.close();
+        driver.quit();
+
+    }
+
+}
