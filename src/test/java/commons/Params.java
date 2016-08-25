e292d57c1619c5d32ea3bfcea3e450442cc52131
HarshenduBhardwaj <harshendubhardwaj@outlook.com>
2016-08-25 16:25:46 -0500

    DL

---
+++ src/test/java/commons/Params.java
@@ -0,0 +1,12 @@
+package commons;
+
+/**
+ * Created by hbhardwa on 7/27/2016.
+ */
+public class Params {
+
+    public static void main(String[] args) {
+
+        // Read parameters from xml
+    }
+}
